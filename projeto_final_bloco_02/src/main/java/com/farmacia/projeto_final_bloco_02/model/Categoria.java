d0af1c313042a77daa895f0ab951013b5180de1a
Thayná Simões <thayna.pereira2402@gmail.com>
2024-12-16 10:29:43 -0300

    Implementação completa do CRUD de Categoria com todos os métodos

---
+++ projeto_final_bloco_02/src/main/java/com/farmacia/projeto_final_bloco_02/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.farmacia.projeto_final_bloco_02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome da categoria é obrigatório!")
+	@Size(min = 2, max = 50)
+	private String nome;
+	
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
---
+++ projeto_final_bloco_02/src/main/java/com/farmacia/projeto_final_bloco_02/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.farmacia.projeto_final_bloco_02.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.farmacia.projeto_final_bloco_02.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	   public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
